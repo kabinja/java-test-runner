bfb579a7c4ecaf8382fa26cd579ec75c26d7f581
kabinja <renaudrwemakika@gmail.com>
2020-08-24 17:27:20 +0200

    add loggers to main class

--- src/main/java/tech/ikora/selenium/locator/evolution/EvolutionAnalysis.java
+++ src/main/java/tech/ikora/selenium/locator/evolution/EvolutionAnalysis.java
@@ -1,19 +1,25 @@
 package tech.ikora.selenium.locator.evolution;
 
 import org.apache.commons.cli.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import tech.ikora.selenium.locator.evolution.configuration.Configuration;
 import tech.ikora.selenium.locator.evolution.configuration.ConfigurationParser;
 
 import java.io.IOException;
 
 public class EvolutionAnalysis {
+    private static final Logger logger = LogManager.getLogger(EvolutionAnalysis.class);
 
     public static void main(String[] args){
         try{
             final Configuration configuration = loadConfiguration(args);
         } catch (Exception e){
-
+            logger.error(String.format("Exit with error: %s", e.getMessage()));
+            System.exit(1);
         }
+
+        logger.info("Finished without error");
     }
 
     static private Configuration loadConfiguration(String[] args) throws ParseException, IOException {
